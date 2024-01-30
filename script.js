e7584ebcab67ffdf5a6dd303cfe6a514a707316b
Noman Jafri <snomanh@hotmail.com>
2024-01-30 08:39:22 +0000

    remove list-style, added checkbox and delete button with list items

--- script.js
+++ script.js
@@ -1,25 +1,29 @@
 //putting DOM connections for manipulation
-let displayTask = document.getElementsByClassName('display-task');
-let task = document.querySelector(".input-task input");
-let newTask;
+const displayTask = document.querySelector('.display-task');
+const task = document.querySelector(".input-task input");
+const newTask = ['<li> <input type="checkbox" id="0"> First<button style="float:right;">Delete</button></li>', '<li> <input type="checkbox" id="1"> Second<button style="float:right;">Delete</button></li>', '<li><input type="checkbox" id="2"> Third<button style="float:right;">Delete</button></li>'];
 
+function updateDisplay() {
+    let html = '' ;
+    newTask.forEach((v, i) => {html = html + v})
+    displayTask.innerHTML ='<ul>' + html + '</ul>';
+}
 
-
-
+updateDisplay();
 
 
 //Putting up Listeners for keyup and click
 task.addEventListener('keyup', event => {
     if (event.key == "Enter"){
-        newTask = task.value;
-        console.log(newTask);
+        newTask.push('<li>' + '<input type="checkbox" id=' + newTask.length +'> '+task.value+'<button style="float:right;">Delete</button></li>');
+        updateDisplay();
         task.value = '';
-    }
-    // console.log(event.key)
+    };
 });
 
 document.querySelector("#push").onclick = (e) => {
     e.preventDefault();
-    console.log(task.value);
+    newTask.push('<li>' + '<input type="checkbox" id=' + newTask.length +'> '+task.value+'<button style="float:right;">Delete</button></li>');
+    updateDisplay();
     task.value = '';
 }
