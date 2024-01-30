cc3e2e385f455bc8c542242a63b6b108e77926d9
Noman Jafri <snomanh@hotmail.com>
2024-01-30 07:14:45 +0000

    DOM Connections both ways getting input and clearing afterwards

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 let displayTask = document.getElementsByClassName('display-task');
-let task = document.getElementById('task');
+let task = document.querySelector(".input-task input");
+let newTask;
 
+
+task.addEventListener('keyup', event => {
+    if (event.key == "Enter"){
+        newTask = task.value;
+        console.log(newTask);
+        task.value = '';
+    }
+    // console.log(event.key)
+});
+
+document.querySelector("#push").onclick = () => {
+    console.log(task.value);
+    task.value = '';
+}
