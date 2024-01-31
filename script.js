93ff99cc590851e29be17b9a229383d60c382a56
Noman Jafri <snomanh@hotmail.com>
2024-01-31 03:56:57 +0000

    Change 'updateDisplay' function to 'checkDelListener' remove unnecessary code and consoleLogs code now Production Ready

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-//putting DOM connections for manipulation
-const displayTask = document.querySelector('.display-task');
+//DOM connection to Add task input field
 const task = document.querySelector(".input-task input");
-var newTask = '<li> <input type="checkbox" id="0"><p>First</p><button class="delete-button"><img src="./assets/delete-button-svgrepo-com.svg" id="0"></button></li><li> <input type="checkbox" id="1"><p>Second</p><button class="delete-button"><img src="./assets/delete-button-svgrepo-com.svg" id="1"></button></li><li><input type="checkbox" id="2"><p> Third </p><button class="delete-button"><img src="./assets/delete-button-svgrepo-com.svg" id="2"></button></li>';
+
 
 //delete button event handler function
 const delEventHandler = function (e) {
@@ -20,39 +19,35 @@ const checkboxHandler = function () {
 }
 
 // Update display when any user interaction took place
-function updateDisplay() {
-    // let html = '' ;
-    // newTask.forEach((v, i) => {html = html + v})    
-    displayTask.innerHTML ='<ul>' + newTask + '</ul>';
+function checkDelListeners() {
+    //Gathering all relevant checkboxes and del buttons from DOM
     const delButtons = document.getElementsByClassName("delete-button");
     const checkInputs = document.querySelectorAll(".display-task input");
-    console.log(checkInputs);
-
+    
     // Remove event listeners important for performance / behaviour
     for (let i = 0; i < delButtons.length; i++) {
         delButtons[i].removeEventListener("click", delEventHandler, true);
         checkInputs[i].removeEventListener('click', checkboxHandler, true);
     }
     
-    // setting up new event listeners on current delete buttons
+    // setting up new event listeners on current checkboxes & delete buttons
     for (let i = 0; i < delButtons.length; i++) {
         delButtons[i].addEventListener("click", delEventHandler, true);
         checkInputs[i].addEventListener('click', checkboxHandler, true);        
     }
 }
 
-// calling update display for first mounting display
-updateDisplay();
-
 //create new task handler function
 function createNewTask(value) {
-    newTask = document.querySelector(".display-task ul").innerHTML
-    newTask += '<li><input type="checkbox"><p>'+value+'</p><button class="delete-button"><img src="./assets/delete-button-svgrepo-com.svg"></button></li>';
-    updateDisplay();
+    const displayTask = document.querySelector('.display-task ul');
+    let newTask = displayTask.innerHTML;
+    newTask += `<li><input type="checkbox"><p>${value}</p><button class="delete-button"><img src="./assets/delete-button-svgrepo-com.svg"></button></li>`;
+    displayTask.innerHTML =`${newTask}`;
+    checkDelListeners();
     task.value = '';
 }
 
-//Putting up Listeners for keyup and click
+//Putting up Listeners for Enter key
 task.addEventListener('keyup', event => {
     if (event.key == "Enter" && task.value != ''){
         createNewTask(task.value);
@@ -61,7 +56,7 @@ task.addEventListener('keyup', event => {
     };
 });
 
-//Putting up Listeners for click
+//Putting up Listeners for click on Add buttom
 document.querySelector("#push").onclick = (e) => {
     e.preventDefault();
     if (task.value != '') {
